7e2f1103728b7e9c4a5e74c5a44231e53a7aa8da
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-06-11 19:32:41 +0100

    feat: correctly importing and transpiling

---
+++ spec/foo.test.ts
@@ -0,0 +1,9 @@
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
+
+describe("foo describe", () => {
+  it("just run", () => {
+    console.log("hello!")
+    console.log(DirectSecp256k1HdWallet)
+    expect(true).toBeTrue()
+  })
+})
