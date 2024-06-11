a1ceead4ec82c3bae43e0c7665e58135a1142816
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-06-11 20:17:59 +0100

    feat: import and use nillion-client-browser

--- spec/foo.test.ts
+++ spec/foo.test.ts
@@ -1,9 +1,14 @@
+import * as nillion from "nillion-client-browser"
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
 
 describe("foo describe", () => {
   it("just run", () => {
     console.log("hello!")
+
     console.log(DirectSecp256k1HdWallet)
+
+    console.log(nillion.NodeKey)
+
     expect(true).toBeTrue()
   })
 })
